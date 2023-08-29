d7d289d155741b42112d298e42f7da54b8efa8a9
yisha23 <112813483+yisha23@users.noreply.github.com>
2023-08-29 12:41:44 +0300

    REMOVE SHIT

--- src/main/java/frc/trigon/robot/subsystems/steer/Steer.java
+++ src/main/java/frc/trigon/robot/subsystems/steer/Steer.java
@@ -8,11 +8,6 @@ import edu.wpi.first.wpilibj2.command.*;
 import java.util.function.Supplier;
 
 public class Steer extends SubsystemBase {
-
-    // With eager singleton initialization, any static variables/fields used in the 
-    // constructor must appear before the "INSTANCE" variable so that they are initialized 
-    // before the constructor is called when the "INSTANCE" variable initializes.
-
     private final static Steer INSTANCE = new Steer();
 
     public static Steer getInstance() {
