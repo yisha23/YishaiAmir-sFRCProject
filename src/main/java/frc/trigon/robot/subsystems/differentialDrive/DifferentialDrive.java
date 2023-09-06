c6a16b1e3fd48eb82833c836c5c75f0a7a2e2a32
yisha23 <yishayamir@gmail.com>
2023-09-06 18:29:49 +0300

    update DifferentialDrive and finish it

--- src/main/java/frc/trigon/robot/subsystems/differentialDrive/DifferentialDrive.java
+++ src/main/java/frc/trigon/robot/subsystems/differentialDrive/DifferentialDrive.java
@@ -1,8 +1,5 @@
 package frc.trigon.robot.subsystems.differentialDrive;
 
-
-import com.ctre.phoenixpro.controls.PositionVoltage;
-import com.ctre.phoenixpro.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -12,43 +9,77 @@ import java.util.function.Supplier;
 public class DifferentialDrive extends SubsystemBase {
 
     private final static DifferentialDrive INSTANCE = new DifferentialDrive();
-
-    private final TalonFX rightFrontMotor = DifferentialDriveConstants.RIGHT_FRONT_MOTOR;
-    private final TalonFX rightRearMotor = DifferentialDriveConstants.RIGHT_REAR_MOTOR;
-    private final TalonFX leftFrontMotor = DifferentialDriveConstants.LEFT_FRONT_MOTOR;
-    private final TalonFX leftRearMotor = DifferentialDriveConstants.LEFT_REAR_MOTOR;
-
-    public static DifferentialDrive getInstance() {
-        return INSTANCE;
-    }
+    private final edu.wpi.first.wpilibj.drive.DifferentialDrive differentialDrive = DifferentialDriveConstants.DIFFERENTIAL_DRIVE;
 
     private DifferentialDrive() {
     }
 
-    public CommandBase getTankDriveCommand(Supplier <Double> rightStickYSupplier, Supplier <Double> lefStickYSupplier){
+    /**
+     * Creates new tank drive command with a given left and right rates.
+     * @param rightStickYSupplier supplies the rates of the right stick
+     * @param lefStickYSupplier supplies the rates of the left stick
+     * @return the command
+     */
+    public CommandBase getTankDriveCommand(Supplier<Double> rightStickYSupplier, Supplier<Double> lefStickYSupplier) {
         return new FunctionalCommand(
-                () ->{},
-                () -> tankDrive(rightStickYSupplier.get(), lefStickYSupplier.get()),
+                () -> {
+                },
+                () -> arcadeDrive(rightStickYSupplier.get(), lefStickYSupplier.get()),
                 (interrupted) -> stop(),
                 () -> false,
                 this
         );
     }
 
-    private void tankDrive(double rightStickY, double lefStickY){
-        PositionVoltage rightRequest = new PositionVoltage(rightStickY);
-        rightFrontMotor.setControl(rightRequest);
-        rightRearMotor.setControl(rightRequest);
-        PositionVoltage leftRequest = new PositionVoltage(lefStickY);
-        leftFrontMotor.setControl(leftRequest);
-        leftRearMotor.setControl(leftRequest);
+    /**
+     * Creates new arcade drive with a given forward and turn rate.
+     * @param driveStickXSupplier The robot's speed along the X axis. Forward is positive
+     * @param driveStickYSupplier The robot's rotation rate around the Y axis
+     * @return the command
+     */
+    public CommandBase getArcadeDriveCommand(Supplier<Double> driveStickXSupplier, Supplier<Double> driveStickYSupplier) {
+        return new FunctionalCommand(
+                () -> {
+                },
+                () -> tankDrive(driveStickXSupplier.get(), driveStickYSupplier.get()),
+                (interrupted) -> stop(),
+                () -> false,
+                this
+        );
     }
 
-    private void stop(){
-        rightFrontMotor.stopMotor();
-        rightRearMotor.stopMotor();
-        leftFrontMotor.stopMotor();
-        leftRearMotor.stopMotor();
+    /**
+     * Creates a new curvature drive command with a given forward and turn rate, as well as a button for turning in-place.
+     * @param driveStickXSupplier The robot's speed along the X axis. Forward is positive
+     * @param driveStickYSupplier The normalized curvature. Counterclockwise is positive
+     * @param allowTurnInPlaceSupplier  If set,the robot allows to turn-in-place
+     * @return the command
+     */
+    public CommandBase getCurvatureDriveCommand(Supplier<Double> driveStickXSupplier, Supplier<Double> driveStickYSupplier, Supplier<Boolean> allowTurnInPlaceSupplier) {
+        return new FunctionalCommand(
+                () -> {
+                },
+                () -> CurvatureDrive(driveStickXSupplier.get(), driveStickYSupplier.get(), allowTurnInPlaceSupplier.get()),
+                (interrupted) -> stop(),
+                () -> false,
+                this
+        );
+    }
+
+    private void tankDrive(double rightStickY, double lefStickY) {
+        differentialDrive.tankDrive(rightStickY, lefStickY);
+    }
+
+    private void arcadeDrive(double driveStickX, double driveStickY) {
+        differentialDrive.arcadeDrive(driveStickX, driveStickY);
+    }
+
+    private void CurvatureDrive(double driveStickX, double driveStickY, boolean allowTurnInPlace){
+        differentialDrive.curvatureDrive(driveStickX, driveStickY ,allowTurnInPlace);
+    }
+
+    private void stop() {
+        differentialDrive.stopMotor();
     }
 
 }
