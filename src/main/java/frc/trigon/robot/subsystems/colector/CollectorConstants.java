b74e659f0849d8974a14f4f54390873930a7541f
yisha23 <yishayamir@gmail.com>
2023-09-07 17:34:36 +0300

    return everything

---
+++ src/main/java/frc/trigon/robot/subsystems/colector/CollectorConstants.java
@@ -0,0 +1,27 @@
+package frc.trigon.robot.subsystems.colector;
+
+import com.ctre.phoenixpro.configs.TalonFXConfiguration;
+import com.ctre.phoenixpro.hardware.TalonFX;
+import com.ctre.phoenixpro.signals.InvertedValue;
+import com.ctre.phoenixpro.signals.NeutralModeValue;
+
+public class CollectorConstants {
+    private static final int MOTOR_ID = 0;
+    private static final InvertedValue INVERTED_VALUE = InvertedValue.CounterClockwise_Positive;
+    private static final NeutralModeValue neutralModeValue = NeutralModeValue.Coast;
+    static final TalonFX MOTOR = new TalonFX(MOTOR_ID);
+
+    static final double
+            COLLECT_VOLTAGE = 6,
+            EJECT_VOLTAGE = -6;
+
+    static {
+        TalonFXConfiguration talonFXConfiguration = new TalonFXConfiguration();
+
+        talonFXConfiguration.Audio.BeepOnBoot = false;
+        talonFXConfiguration.MotorOutput.Inverted = INVERTED_VALUE;
+        talonFXConfiguration.MotorOutput.NeutralMode = neutralModeValue;
+
+        MOTOR.getConfigurator().apply(talonFXConfiguration);
+    }
+}
---
+++ src/main/java/frc/trigon/robot/subsystems/differentialDrive/DifferentialDrive.java
@@ -0,0 +1,86 @@
+package frc.trigon.robot.subsystems.differentialDrive;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.util.function.Supplier;
+
+public class DifferentialDrive extends SubsystemBase {
+
+    private final static DifferentialDrive INSTANCE = new DifferentialDrive();
+    private final edu.wpi.first.wpilibj.drive.DifferentialDrive differentialDrive = DifferentialDriveConstants.DIFFERENTIAL_DRIVE;
+
+    private DifferentialDrive() {
+    }
+
+    /**
+     * Creates new tank drive command with a given left and right rates.
+     * @param rightSpeedSupplier supplies the rates of the right stick
+     * @param leftSpeedSupplier supplies the rates of the left stick
+     * @return the command
+     */
+    public CommandBase getTankDriveCommand(Supplier<Double> rightSpeedSupplier, Supplier<Double> leftSpeedSupplier, boolean squareInput) {
+        return new FunctionalCommand(
+                () -> {
+                },
+                () -> tankDrive(leftSpeedSupplier.get(), rightSpeedSupplier.get(), squareInput),
+                (interrupted) -> stop(),
+                () -> false,
+                this
+        );
+    }
+
+    /**
+     * Creates new arcade drive with a given forward and turn rate.
+     * @param speedSupplier The robot's speed along the X axis. Forward is positive
+     * @param rotationSupplier The robot's rotation rate around the Y axis
+     * @return the command
+     */
+    public CommandBase getArcadeDriveCommand(Supplier<Double> speedSupplier, Supplier<Double> rotationSupplier, boolean squareInput) {
+        return new FunctionalCommand(
+                () -> {
+                },
+                () -> arcadeDrive(speedSupplier.get(), rotationSupplier.get(), squareInput),
+                (interrupted) -> stop(),
+                () -> false,
+                this
+        );
+    }
+
+    /**
+     * Creates a new curvature drive command with a given forward and turn rate, as well as a button for turning in-place.
+     * @param speedSupplier The robot's speed along the X axis. Forward is positive
+     * @param rotationSupplier The normalized curvature. Counterclockwise is positive
+     * @param allowTurnInPlace  If set,the robot allows to turn-in-place
+     * @return the command
+     */
+    public CommandBase getCurvatureDriveCommand(Supplier<Double> speedSupplier, Supplier<Double> rotationSupplier, boolean allowTurnInPlace) {
+        return new FunctionalCommand(
+                () -> {
+                },
+                () -> CurvatureDrive(speedSupplier.get(), rotationSupplier.get(), allowTurnInPlace),
+                (interrupted) -> stop(),
+                () -> false,
+                this
+        );
+    }
+
+    private void tankDrive(double lefSpeed, double rightSpeed, boolean squareInput) {
+        differentialDrive.tankDrive(lefSpeed, rightSpeed, squareInput);
+    }
+
+    private void arcadeDrive(double speed, double rotation, boolean squareInput) {
+        differentialDrive.arcadeDrive(speed, rotation, squareInput);
+    }
+
+    private void CurvatureDrive(double speed, double rotation, boolean allowTurnInPlace){
+        differentialDrive.curvatureDrive(speed, rotation ,allowTurnInPlace);
+    }
+
+    private void stop() {
+        differentialDrive.stopMotor();
+    }
+
+}
+
