2ebc607426169334795f7ebd78edafbfb4db6c7b
yisha23 <yishayamir@gmail.com>
2023-09-11 17:55:02 +0300

    change from talonfx to talonSRx

--- src/main/java/frc/trigon/robot/subsystems/differentialDrive/DifferentialDriveConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/differentialDrive/DifferentialDriveConstants.java
@@ -1,11 +1,11 @@
 package frc.trigon.robot.subsystems.differentialDrive;
 
-import com.ctre.phoenixpro.configs.TalonFXConfiguration;
-import com.ctre.phoenixpro.hardware.TalonFX;
-import com.ctre.phoenixpro.signals.InvertedValue;
-import com.ctre.phoenixpro.signals.NeutralModeValue;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+
+import java.util.List;
 
 public class DifferentialDriveConstants {
     private static final int
@@ -13,28 +13,29 @@ public class DifferentialDriveConstants {
                 RIGHT_REAR_MOTOR_ID = 1,
                 LEFT_FRONT_MOTOR_ID = 2,
                 LEFT_REAR_MOTOR_ID = 3;
-    private static final InvertedValue INVERTED_VALUE = InvertedValue.CounterClockwise_Positive;
-    private static final NeutralModeValue NEUTRAL_MODE_VALUE = NeutralModeValue.Coast;
-    private static final TalonFX
-                RIGHT_FRONT_MOTOR = new TalonFX(RIGHT_FRONT_MOTOR_ID),
-                RIGHT_REAR_MOTOR = new TalonFX(RIGHT_REAR_MOTOR_ID),
-                LEFT_FRONT_MOTOR = new TalonFX(LEFT_FRONT_MOTOR_ID),
-                LEFT_REAR_MOTOR = new TalonFX(LEFT_REAR_MOTOR_ID);
+    private static final boolean INVERTED_VALUE = false;
+    private static final NeutralMode NEUTRAL_MODE_VALUE = NeutralMode.Coast;
+    private static final WPI_TalonSRX
+            RIGHT_FRONT_MOTOR = new WPI_TalonSRX(RIGHT_FRONT_MOTOR_ID),
+            RIGHT_REAR_MOTOR = new WPI_TalonSRX(RIGHT_REAR_MOTOR_ID),
+            LEFT_FRONT_MOTOR = new WPI_TalonSRX(LEFT_FRONT_MOTOR_ID),
+            LEFT_REAR_MOTOR = new WPI_TalonSRX(LEFT_REAR_MOTOR_ID);
+    //private static final TalonFX
+                //RIGHT_FRONT_MOTOR = new TalonFX(RIGHT_FRONT_MOTOR_ID),
+                //RIGHT_REAR_MOTOR = new TalonFX(RIGHT_REAR_MOTOR_ID),
+                //LEFT_FRONT_MOTOR = new TalonFX(LEFT_FRONT_MOTOR_ID),
+                //LEFT_REAR_MOTOR = new TalonFX(LEFT_REAR_MOTOR_ID);
     private static final MotorControllerGroup
                 RIGHT_MOTORS = new MotorControllerGroup(RIGHT_FRONT_MOTOR, RIGHT_REAR_MOTOR),
                 LEFT_MOTORS = new MotorControllerGroup(LEFT_FRONT_MOTOR, LEFT_REAR_MOTOR);
     static final DifferentialDrive DIFFERENTIAL_DRIVE = new DifferentialDrive(RIGHT_MOTORS, LEFT_MOTORS);
 
     static {
-        TalonFXConfiguration talonFXConfiguration = new TalonFXConfiguration();
-
-        talonFXConfiguration.Audio.BeepOnBoot = false;
-        talonFXConfiguration.MotorOutput.Inverted = INVERTED_VALUE;
-        talonFXConfiguration.MotorOutput.NeutralMode = NEUTRAL_MODE_VALUE;
-
-        RIGHT_FRONT_MOTOR.getConfigurator().apply(talonFXConfiguration);
-        RIGHT_REAR_MOTOR.getConfigurator().apply(talonFXConfiguration);
-        LEFT_FRONT_MOTOR.getConfigurator().apply(talonFXConfiguration);
-        LEFT_REAR_MOTOR.getConfigurator().apply(talonFXConfiguration);
+        for (WPI_TalonSRX currentMotor : List.of(RIGHT_FRONT_MOTOR, RIGHT_REAR_MOTOR, LEFT_FRONT_MOTOR, LEFT_REAR_MOTOR)){
+            currentMotor.setInverted(INVERTED_VALUE);
+            currentMotor.setNeutralMode(NEUTRAL_MODE_VALUE);
+        }
+        RIGHT_FRONT_MOTOR.setInverted(INVERTED_VALUE);
+        RIGHT_FRONT_MOTOR.setNeutralMode(NEUTRAL_MODE_VALUE);
     }
 }
