20bb2b6b03fbc0ac647785523433ff38b21cfe2d
yisha23 <yishayamir@gmail.com>
2023-09-11 17:57:23 +0300

    format the class

--- src/main/java/frc/trigon/robot/subsystems/differentialDrive/DifferentialDriveConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/differentialDrive/DifferentialDriveConstants.java
@@ -9,10 +9,10 @@ import java.util.List;
 
 public class DifferentialDriveConstants {
     private static final int
-                RIGHT_FRONT_MOTOR_ID = 0,
-                RIGHT_REAR_MOTOR_ID = 1,
-                LEFT_FRONT_MOTOR_ID = 2,
-                LEFT_REAR_MOTOR_ID = 3;
+            RIGHT_FRONT_MOTOR_ID = 0,
+            RIGHT_REAR_MOTOR_ID = 1,
+            LEFT_FRONT_MOTOR_ID = 2,
+            LEFT_REAR_MOTOR_ID = 3;
     private static final boolean INVERTED_VALUE = false;
     private static final NeutralMode NEUTRAL_MODE_VALUE = NeutralMode.Coast;
     private static final WPI_TalonSRX
@@ -21,12 +21,12 @@ public class DifferentialDriveConstants {
             LEFT_FRONT_MOTOR = new WPI_TalonSRX(LEFT_FRONT_MOTOR_ID),
             LEFT_REAR_MOTOR = new WPI_TalonSRX(LEFT_REAR_MOTOR_ID);
     private static final MotorControllerGroup
-                RIGHT_MOTORS = new MotorControllerGroup(RIGHT_FRONT_MOTOR, RIGHT_REAR_MOTOR),
-                LEFT_MOTORS = new MotorControllerGroup(LEFT_FRONT_MOTOR, LEFT_REAR_MOTOR);
+            RIGHT_MOTORS = new MotorControllerGroup(RIGHT_FRONT_MOTOR, RIGHT_REAR_MOTOR),
+            LEFT_MOTORS = new MotorControllerGroup(LEFT_FRONT_MOTOR, LEFT_REAR_MOTOR);
     static final DifferentialDrive DIFFERENTIAL_DRIVE = new DifferentialDrive(RIGHT_MOTORS, LEFT_MOTORS);
 
     static {
-        for (WPI_TalonSRX currentMotor : List.of(RIGHT_FRONT_MOTOR, RIGHT_REAR_MOTOR, LEFT_FRONT_MOTOR, LEFT_REAR_MOTOR)){
+        for (WPI_TalonSRX currentMotor : List.of(RIGHT_FRONT_MOTOR, RIGHT_REAR_MOTOR, LEFT_FRONT_MOTOR, LEFT_REAR_MOTOR)) {
             currentMotor.setInverted(INVERTED_VALUE);
             currentMotor.setNeutralMode(NEUTRAL_MODE_VALUE);
         }
